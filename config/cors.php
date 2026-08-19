9a292ed987d081289329512c7a875d36a72ca2b0
Sumit Singh <sumitrajput7079@gmail.com>
2026-08-20 00:06:05 +0530

    Update cors file

--- config/cors.php
+++ config/cors.php
@@ -20,13 +20,14 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://crewline-frontend-production.up.railway.app',
-    ],
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://crewline-frontend-production.up.railway.app',
+],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
 
     'max_age' => 0,
@@ -34,3 +35,4 @@ return [
     'supports_credentials' => false,
 
 ];
+
